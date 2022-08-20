624e67987d40904e7b41eb3594cb25bd08fc2b80
AdrianKCL <92375484+AdrianKCL@users.noreply.github.com>
2022-08-20 23:11:57 +0200

    Some changes

--- components/LandingFacts.jsx
+++ components/LandingFacts.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
-import { useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/react";
 import logowhite from "../assets/lilywhite.png";
 import logoblack from "../assets/lilyblack.png";
 
 export default function LandingFacts() {
   const logo = useColorModeValue(logoblack, logowhite);
+  const bg = useColorModeValue("#f7f3ed", "#2D3748");
   return (
-    <Flex width="100%" pt="24px">
+    <Flex width="100%" pt="24px" h="100vh">
       <Flex maxW="500px" m="0 auto">
         <Flex flexDirection="column">
           <Flex w="250px">
             <Image src={logo} />
           </Flex>
-          <Text textAlign="center">Facts about Water Lilies</Text>
+          <Text
+            textAlign="center"
+            background={bg}
+            borderRadius="0.5rem"
+            p="0.75rem"
+            mt="24px"
+          >
+            Facts about Water Lilies
+          </Text>
         </Flex>
       </Flex>
     </Flex>
